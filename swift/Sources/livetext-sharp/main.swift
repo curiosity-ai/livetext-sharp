e13be0141cbc6ff59627e8122de5144d9016aa96
theolivenbaum <rafael@curiosity.ai>
2022-04-17 10:23:07 +0200

    build swift code on CI

--- swift/Sources/livetext-sharp/main.swift
+++ swift/Sources/livetext-sharp/main.swift
@@ -4,11 +4,6 @@ import Cocoa
 import Vision
 
 var joiner = " "
-var bigSur = false;
-
-if #available(OSX 11, *) {
-    bigSur = true;
-}
 
 func convertCIImageToCGImage(inputImage: CIImage) -> CGImage? {
     let context = CIContext(options: nil)
@@ -20,63 +15,44 @@ func convertCIImageToCGImage(inputImage: CIImage) -> CGImage? {
 
 @available(macOS 10.15, *)
 func recognizeTextHandler(request: VNRequest, error: Error?) {
-    guard let observations =
-            request.results as? [VNRecognizedTextObservation] else {
-        return
-    }
-    let recognizedStrings = observations.compactMap { observation in
-        // Return the string of the top VNRecognizedText instance.
-        return observation.topCandidates(1).first?.string
-    }
-    
-    // Process the recognized strings.
+    guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+    let recognizedStrings = observations.compactMap { observation in return observation.topCandidates(1).first?.string }
     let joined = recognizedStrings.joined(separator: joiner)
     print(joined)
-    
-    let pasteboard = NSPasteboard.general
-    pasteboard.declareTypes([.string], owner: nil)
-    pasteboard.setString(joined, forType: .string)
-    
 }
 
 @available(macOS 10.15, *)
-func detectText(fileName : URL) -> [CIFeature]? {
-    if let ciImage = CIImage(contentsOf: fileName){
-        guard let img = convertCIImageToCGImage(inputImage: ciImage) else { return nil}
+func detectText(fileName : URL, languages : [String]) {
+    if let ciImage = CIImage(contentsOf: fileName) 
+    {
+        guard let img = convertCIImageToCGImage(inputImage: ciImage) else { return }
       
         let requestHandler = VNImageRequestHandler(cgImage: img)
-
-        // Create a new request to recognize text.
-        let request = VNRecognizeTextRequest(completionHandler: recognizeTextHandler)
-        request.recognitionLanguages = recognitionLanguages
-       
         
-        do {
-            // Perform the text-recognition request.
+        let request = VNRecognizeTextRequest(completionHandler: recognizeTextHandler)
+
+        request.recognitionLanguages = languages
+        
+        do 
+        {
             try requestHandler.perform([request])
-        } catch {
-            print("Unable to perform the requests: \(error).")
+        }
+        catch 
+        {
+            print("Error: \(error)")
+            exit(0xDEAD)
         }
 }
-    return nil
+    return
 }
 
 let arguments: [String] = Array(CommandLine.arguments.dropFirst())
-
-let inputFile: String = arguments[0]
-
+let inputFile: String = arguments[0].trimmingCharacters(in: CharacterSet(charactersIn: "\""))
 let language: String = arguments[1]
 
-//"/Users/rafael/Downloads/243903.png" en-US
-
 let inputURL = URL(fileURLWithPath: inputFile)
-var recognitionLanguages = [language]
+var recognitionLanguages = language.components(separatedBy: ";")
 
-do {
-    if let features = detectText(fileName : inputURL), !features.isEmpty{}
-
-} catch {
-    print("Whoops! An error occurred: \(error)")
-}
+detectText(fileName : inputURL, languages: recognitionLanguages)
 
 exit(EXIT_SUCCESS)
